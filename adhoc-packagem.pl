546a33fcb145036896a65f510b8a7bbd87da29c9
Sławomir Nizio <poczta-sn@gazeta.pl>
2011-01-17 01:45:10 +0100

    A number of improvements.
    these include:
    - file owner, group and permissions are saved
    - better args parsing
    - changed "rm" to "del" (avoids confusion)
    - added file "docs" with... docs
    - able to specify root path for "del"
    - able to specify own file name for output file
    - "pretend" mode more useful with "del"

--- adhoc-packagem.pl
+++ adhoc-packagem.pl
@@ -17,30 +17,40 @@ use constant {
 my $ext = "insfiles";
 sub usage {
 	my $ishelp = shift;
-	say "args: inst [-p] source_dir [root_path]";
-	say "or: rm [-p] anything.$ext [root_path]";
-	say "or: [-h|--help]";
-	say "example:\t$0 inst stuff";
-	say "example 2:\t$0 rm stuff.$ext";
-	say "example with -p (pretend) and another root path: $0 inst -p stuff /usr/local";
+	# inst [-p] /home/x/bash [-r /tmp] [-p] [-file insfile]
+	# del [-p] insfile [-r /tmp]
+	say "args: inst [-p] source_dir [-r root_path] [-file output]";
+	say "or:   del [-p] input.$ext [-r root_path]";
+	say "or:   [-h|--help]";
+	say "";
+	say "examples:";
+	say "a)  $0 inst /tmp/lxdm -file /home/me/file";
+	say "    will install (copy) stuff from /tmp/lxdm to / and create file /home/me/file.$ext";
+	say "b)  $0 inst -p /tmp/lxdm -r /mnt/abc/usr";
+	say "    will install stuff from /tmp/lxdm to /mnt/abc/usr and create file /tmp/lxdm.$ext";
+	say "c)  $0 del /tmp/lxdm.$ext -r /";
+	say "    will remove stuff as described in /tmp/lxdm.$ext; it's the same as";
+	say "    \"$0 del /tmp/lxdm.$ext\" because / is default root directory";
 	say "\nUse --help to get some more info." unless defined $ishelp;
 }
 
 sub help {
-	say "\"Ad hoc package manager\": this installs files from a source to \"root\" directory (/ by default)";
-	say "and helps uninstalling them";
-	say "for this purpose, a file .$ext is made";
-	say "only contents of source directory are copied, not the directory itself";
+	say "\"Ad hoc package manager\": this installs files from a source to \"root\"";
+	say "directory (/ by default) and helps uninstalling them.";
+	say "For this purpose, a file .$ext is made.";
+	say "Only contents of source directory are copied, not the directory itself.";
 	say "";
-	say "if a file exists, it's not overridden - copying is aborted";
-	say "*** and file modes are ignored ***";
+	say "With \"inst\" if a file exists, it's not overridden - copying is aborted.";
 	say "";
 	say "In pretend mode (-p) no files are copied or removed, but with \"inst\"";
-	say "command an .$ext file is written as it would be.";
+	say "command an .$ext file is written as it would be, and with \"del\"";
+	say "command the list of files and directories with be printed, with the";
+	say "information whether they still exist or not.";
 	say "";
-	say "warning: it's not designed to be completely error-prone";
-	say "esp. if one gives wrong args (source or destination)";
+	say "Warning: it's not designed to be completely error-prone";
+	say "esp. if one gives wrong args (source or destination).";
 	say "";
+	say "For more info see included \"docs\" file.";
 	# files with \n in name? `.', `' or anything wrong as src/dest? the same REAL src/dest?
 	say "\nauthor: Enlik";
 	say "";
@@ -52,7 +62,7 @@ my $pretend = 0; # 0 or 1
 my $source;
 my $root; # absolute path, with a / at the end
 my $ff_fh;
-my $ff;
+my $ff; # output file
 
 if ($^O ne "linux") {
 	say "Sorry, your OS may not be supported.";
@@ -61,54 +71,7 @@ if ($^O ne "linux") {
 	exit EXIT_CANTCONT;
 }
 
-{
-	my ($oper, $_source);
-	$oper = shift // "";
-	$_source = shift;
-	if(defined $_source and $_source eq "-p") {
-		$pretend = 1;
-		$_source = shift;
-	}
-	
-	if ($oper eq "inst") {
-		$inst = 1;
-		$root = shift || "/"; # yeah, also for empty string
-		unless (defined $_source) {
-			say "Error: you didn't provide a source dir.\n";
-			usage;
-			exit EXIT_WRARG;
-		}
-		if(@ARGV) {
-			say "Error: too much parameters.\n";
-			usage;
-			exit EXIT_WRARG;
-		}
-		$source = $_source;
-	}
-	elsif ($oper eq "rm") {
-		$inst = 0;
-		unless (defined $_source) {
-			say "Error: you didn't provide a .$ext file.\n";
-			usage;
-			exit EXIT_WRARG;
-		}
-		if(@ARGV) {
-			say "Error: too much parameters.\n";
-			usage;
-			exit EXIT_WRARG;
-		}
-		$source = $_source;
-	}
-	elsif ($oper eq "-h" or $oper eq "--help") {
-		help;
-		exit EXIT_OK;
-	}
-	else {
-		say "Error: incorrect args.\n";
-		usage;
-		exit EXIT_WRARG;
-	}
-}
+parse_cmdline();
 
 if($pretend) {
 	say "Running in \"pretend\" mode.";
@@ -118,7 +81,7 @@ if ($inst) {
 	do_inst();
 }
 else {
-	do_rm();
+	do_del();
 }
 
 #################################### inst ####################################
@@ -141,16 +104,20 @@ sub do_inst {
 	}
 	say "info: source is: $source";
 	say "info: destination is: $root";
-	$ff = $source . "." . $ext;
+	if (defined $ff) { # user provided his own
+		$ff .= "." . $ext unless $ff =~ /.+\.\Q$ext\E$/
+	}
+	else {
+		$ff = $source . "." . $ext;
+	}
 	if (-f $ff) {
 		say "Error, file $ff already exists.";
-		say "Have you already installed somewhere the files?";
+		say "Have you already installed the files somewhere?";
 		exit EXIT_CANTCONT;
 	} 
 	else {
 		say ".$ext file is: $ff";
 	}
-	
 	# chdir $source or die "Can't change dir to $source: $!\n";
 	
 	unless (open ($ff_fh, ">", $ff)) {
@@ -163,6 +130,7 @@ sub do_inst {
 	say $ff_fh "# $source -> $root";
 	say $ff_fh "VERSION 1";
 	say $ff_fh "ROOTDIR $root";
+	# todo: if find reports errors "can't cd to...", it is not handled
 	find (\&process_file, $source) or exit EXIT_CANTCONT;
 	close $ff_fh;
 	say "\nDone. Remember to backup the file $ff.";
@@ -192,7 +160,7 @@ sub process_file {
 		elsif(-e _) {
 			say "Error! The file $dst_path exists and it's not a directory.";
 			say "Aborting!";
-			say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 rm $ff";
+			say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 del $ff";
 			close $ff_fh;
 			exit EXIT_ERRPROC; # with File::Find one can't abort find() it seems :/
 		}
@@ -201,9 +169,17 @@ sub process_file {
 				unless (mkdir $dst_path) {
 					say "Error! Can't create directory $dst_path - aborting!";
 					say $!;
-					say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 rm $ff";
+					say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 del $ff";
 					close $ff_fh;
 					exit EXIT_ERRPROC;
+				}
+				my $ret = clone_modes($file, $dst_path);
+				if ($ret < 0) {
+					given($ret) {
+						say "Warning: can't stat $file: $!" when (-1);
+						say "Warning: can't change mode for $file: $!" when (-2);
+						say "Warning: can't change owner/group for $file: $!" when (-3);
+					}
 				}
 			}
 			say $ff_fh "NEWDIR $dst_path";
@@ -213,7 +189,7 @@ sub process_file {
 		if(-d $dst_path) {
 			say "Error! The file $dst_path exists and IS a directory,";
 			say "but source file is not a directory. Aborting!";
-			say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 rm $ff";
+			say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 del $ff";
 			close $ff_fh;
 			exit EXIT_ERRPROC;
 		}
@@ -221,7 +197,7 @@ sub process_file {
 			# let's yell a bit
 			say "Error! The file $dst_path exists!";
 			say "I'm not going to overwrite it. Aborting!";
-			say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 rm $ff";
+			say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 del $ff";
 			close $ff_fh;
 			exit EXIT_ERRPROC;
 		}
@@ -231,9 +207,17 @@ sub process_file {
 				unless (copy $file, $dst_path) {
 					say "Error! Can't copy file $path to $dst_path - aborting!";
 					say $!;
-					say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 rm $ff";
+					say "REMEMBER TO UNDONE CHANGES MANUALLY using $0 del $ff";
 					close $ff_fh;
 					exit EXIT_ERRPROC;
+				}
+				my $ret = clone_modes($file, $dst_path);
+				if ($ret < 0) {
+					given($ret) {
+						say "Warning: can't stat $file: $!" when (-1);
+						say "Warning: can't change mode for $file: $!" when (-2);
+						say "Warning: can't change owner/group for $file: $!" when (-3);
+					}
 				}
 			}
 			say $ff_fh "NEWFILE $dst_path";
@@ -241,21 +225,49 @@ sub process_file {
 	}
 }
 
-#################################### rm ####################################
+# Oh sweet!
+sub clone_modes {
+	my ($from, $to) = @_;
+	return if(!defined $from or !defined $to);
+	# note: change to lstat() if it treats symlinks as symlinks in the future
+	my ($dev, $mode, $uid, $gid) = (stat ($from))[0,2,4,5];
+	unless (defined $dev) {
+		return -1;
+	}
+	$mode = $mode & 07777;
 
-sub do_rm {
-	unless ($source =~ /.+\.\Q$ext\E$/) {
-		say "The file should end with .$ext.";
+	unless(chmod $mode, $to) {
+		return -2;
+	}
+	unless (chown $uid, $gid, $to) {
+		return -3;
+	}
+	return 0;
+}
+
+#################################### del ####################################
+
+sub do_del {
+	undef $source; # easier to find bugs in code when using $source instead of $ff by accident
+	unless ($ff =~ /.+\.\Q$ext\E$/) {
+		say "The file $ff should end with .$ext.";
 		exit EXIT_WRARG;
 	}
-	unless (open $ff_fh, "<", $source) {
-		say "I can't open $source for reading: $!.";
+	unless (open $ff_fh, "<", $ff) {
+		say "I can't open $ff for reading: $!.";
 		exit EXIT_CANTCONT;
 	}
 	my @opers; # $opers[n] = { oper => NEW...,  path => ... }
 	my $version;
 	my $line = 0;
 	my $rootdir; # unused, even not checked
+	my $my_root = "";
+	# note to Perl scripters (including myself): if (! $x eq "/") is wrong, negates $x ('not' is OK)
+	unless($root eq "/") {
+		$my_root = $root; # set root directory (all removed items will be its subdirectory)
+		if ($my_root =~ /(.+?)\/+$/) { $my_root = $1 } # rstrip /s
+		say "Root directory for the operation is $my_root.";
+	}
 	
 	while (<$ff_fh>) {
 		$line++;
@@ -288,25 +300,120 @@ sub do_rm {
 			exit EXIT_ERRPROC;
 		}
 	}
+	close $ff_fh;
 	
-	unless($pretend) {
-		@opers = reverse @opers;
-		for (@opers) {
-			if ($_->{oper} eq 'NEWFILE') {
-				unless (unlink $_->{path}) {
-					say "Warning: can't remove file $_->{path}.";
+	my $path;
+	@opers = reverse @opers;
+	for (@opers) {
+		$path = $my_root . $_->{path};
+		if ($_->{oper} eq 'NEWFILE') {
+			unless ($pretend) {
+				unless (unlink $path) {
+					say "Warning: can't remove file $path.";
 					say "\t$!";
 				}
 			}
-			elsif ($_->{oper} eq 'NEWDIR') {
-				unless (rmdir $_->{path}) {
-					say "Warning: can't remove directory $_->{path}.";
+			else {
+				say "[FILE] " . $path;
+				say "\tInfo: this file doesn't exist or is not a regular file."
+					unless -f $path;
+			}
+		}
+		elsif ($_->{oper} eq 'NEWDIR') {
+			unless($pretend) {
+				unless (rmdir $path) {
+					say "Warning: can't remove directory $path.";
 					say "\t$!";
-					say "\tMaybe it wasn't empty because anything was added";
+					say "\tMaybe it wasn't empty because something was added";
 					say "\tto it after using this tool.";
 				}
 			}
+			else {
+				say " [DIR] " . $path;
+				say "\tInfo: this directory doesn't exist or the item is not a directory."
+					unless -d $path;
+			}
 		}
 	}
-	say "Done! You may remove $source if you want. Enjoy!";
+	say "Done! You may remove $ff if you want. Enjoy!" unless $pretend;
+}
+
+#################################### parse_cmdline ####################################
+
+sub parse_cmdline {
+	my $oper = shift @ARGV;
+	$source = shift @ARGV;
+	if(defined $source and $source eq "-p") {
+		$pretend = 1;
+		$source = shift @ARGV;
+	}
+	given($oper) {
+		when ("inst") {
+			$inst = 1;
+			unless (defined $source) {
+				say "Error: you didn't provide a source dir.\n";
+				usage;
+				exit EXIT_WRARG;
+			}
+		}
+		when("del") {
+			$inst = 0;
+			unless (defined $source) {
+				say "Error: you didn't provide a .$ext file.\n";
+				usage;
+				exit EXIT_WRARG;
+			}
+		}
+		when(["-h", "--help"]) {
+			help;
+			exit EXIT_OK;
+		}
+		
+		say "Error: incorrect args.\n";
+		usage;
+		exit EXIT_WRARG;
+	}
+	
+	say "Warning, source directory is $source and begins with a `-' - make sure\n" .
+		"it's not due to a mistake in parameters.\n"
+		if ($source =~ /^-/);
+	
+	while(my $arg = shift @ARGV) {
+		given ($arg) {
+			when("-p") {
+				say "Warning, -p (pretend) is already specified." if $pretend;
+				$pretend = 1;
+			}
+			when("-r") {
+				say "Warning, -r DIR specified once again, overriding." if defined $root;
+				$root = shift @ARGV;
+				if (!defined $root or $root eq "") {
+					say "You should provide root (destination) directory name after -r.";
+					exit EXIT_WRARG;
+				}
+			}
+			when("-file") {
+				if(!$inst) {
+					say "Warning, source file overridden by -file option."
+				}
+				elsif (defined $ff) {
+					say "Warning, source file overriding by -file option."
+				}
+				$ff = shift @ARGV;
+				if (!defined $ff or $ff eq "") {
+					say "You should provide a .$ext file name after -file.";
+					exit EXIT_WRARG;
+				}
+			}
+			when(["-h", "--help"]) {
+				help;
+				exit EXIT_OK;
+			}
+			say "Error: too much or wrong parameters ($arg).\n";
+			usage;
+			exit EXIT_WRARG;
+		}
+	}
+	$root = $root // "/";
+	$ff = $ff // $source if(!$inst);
 }
